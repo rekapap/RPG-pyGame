e48183ce38887d9bb3e1cad2414600f939261474
Reka <rekapap28@gmail.com>
2019-02-08 10:47:18 +0000

    adds game screen

--- pyGame.py
+++ pyGame.py
@@ -1,8 +1,12 @@
 import pygame
-#  SCREEN SETUP
+#  SCREEN SIZE
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
 # COLORS
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = ( 0, 0, 0)
+
+# initalising game_screen
+game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+game_screen.fill(WHITE_COLOR)
