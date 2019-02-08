efe07b7f01394a755eba8a7ed247a6180c5c28e5
Reka <rekapap28@gmail.com>
2019-02-08 11:15:42 +0000

    adds game while loop

--- pyGame.py
+++ pyGame.py
@@ -1,12 +1,33 @@
 import pygame
-#  SCREEN SIZE
+
+pygame.init()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
+SCREEN_TITLE = 'RPG'
 
 # COLORS
 WHITE_COLOR = (255, 255, 255)
-BLACK_COLOR = ( 0, 0, 0)
+BLACK_COLOR = (0, 0, 0)
+
+clock = pygame.time.Clock()
+TICK_RATE = 60
+is_game_over = False
 
 # initalising game_screen
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE_COLOR)
+pygame.display.set_caption(SCREEN_TITLE)
+
+while not is_game_over:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+                print(event)
+
+            pygame.display.update()
+            clock.tick(TICK_RATE)
+
+pygame.quit()
+quit()
