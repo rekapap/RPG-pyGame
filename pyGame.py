44b3326660461c673fb4d2ed49597c3a5e6d01ab
Reka <rekapap28@gmail.com>
2019-02-12 21:54:20 +0000

    refactores game to have game class

--- pyGame.py
+++ pyGame.py
@@ -1,33 +1,37 @@
 import pygame
 
-pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = 'Crossy RPG'
 WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
-
 clock = pygame.time.Clock()
-TICK_RATE = 60
-is_game_over = False
-
-game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-game_screen.fill(WHITE_COLOR)
-pygame.display.set_caption(SCREEN_TITLE)
-
 pygame.font.init()
 font = pygame.font.SysFont('Impact', 75)
 
+class Game:
+    TICK_RATE = 60
+    def __init__(self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height
+        self.game_screen = pygame.display.set_mode((width, height))
+        self.game_screen.fill(WHITE_COLOR)
+        pygame.display.set_caption(title)
 
-while not is_game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_game_over = True
-        print(event)
+    def run_game_loop(self):
+        is_game_over = False
+        while not is_game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+                print(event)
 
-    pygame.display.update()
-    clock.tick(TICK_RATE)
+            pygame.display.update()
+            clock.tick(self.TICK_RATE)
 
+pygame.init()
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game_loop()
 pygame.quit()
 quit()
