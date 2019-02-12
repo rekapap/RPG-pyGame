438fadd47de62efb8f191ed0d0ed22206abfb10a
Reka <rekapap28@gmail.com>
2019-02-12 11:34:38 +0000

    adds font removes images

--- pyGame.py
+++ pyGame.py
@@ -16,8 +16,8 @@ game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE_COLOR)
 pygame.display.set_caption(SCREEN_TITLE)
 
-player_image = pygame.image.load('./assets/player.png')
-player_image = pygame.transform.scale(player_image, (50, 50))
+pygame.font.init()
+font = pygame.font.SysFont('Impact', 75)
 
 
 while not is_game_over:
@@ -26,7 +26,6 @@ while not is_game_over:
             is_game_over = True
         print(event)
 
-    game_screen.blit(player_image, (375, 375))
     pygame.display.update()
     clock.tick(TICK_RATE)
 
