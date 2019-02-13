dcc6362ecab45d7dada8f17dc8a4b1b663f96a97
Reka <rekapap28@gmail.com>
2019-02-13 23:10:46 +0000

    adds playercharacter

--- pyGame.py
+++ pyGame.py
@@ -21,14 +21,28 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
+        player_character = PlayerCharacter('./assets/player.png', 375, 700, 50, 50)
         while not is_game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_game_over = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+
                 print(event)
+                player_character.move(direction)
+                player_character.draw(self.game_screen)
 
             pygame.display.update()
             clock.tick(self.TICK_RATE)
+
 class GameObject:
     def __init__(self, image_path, x, y, width,  height):
         object_image = pygame.image.load(image_path)
@@ -38,6 +52,17 @@ class GameObject:
 
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
+
+class PlayerCharacter(GameObject):
+    SPEED = 10
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
 
 pygame.init()
 new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
