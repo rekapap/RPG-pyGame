1825e6683183157dea89ebd5e83970509a32629e
Reka <rekapap28@gmail.com>
2019-02-13 22:59:02 +0000

    adds gameobejct class

--- pyGame.py
+++ pyGame.py
@@ -29,6 +29,15 @@ class Game:
 
             pygame.display.update()
             clock.tick(self.TICK_RATE)
+class GameObject:
+    def __init__(self, image_path, x, y, width,  height):
+        object_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(object_image, (width, height))
+        self.x_pos = x
+        self.y_pos = y
+
+    def draw(self, background):
+        background.blit(self.image, (self.x_pos, self.y_pos))
 
 pygame.init()
 new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
